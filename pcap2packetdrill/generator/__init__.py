633888b191f160fa44e171fb6652ce54f915397f
Martin Becke <Martin.Becke@HAW-Hamburg.de>
2025-04-10 09:41:28 +0200

    Create generator package __init__.py

---
+++ pcap2packetdrill/generator/__init__.py
@@ -0,0 +1,16 @@
+"""
+Replay test script generator package for PCAP2Packetdrill.
+
+This package provides functionality for generating protocol-specific replay test scripts 
+from packet captures.
+"""
+
+from pcap2packetdrill.generator.tcp_generator import TCPReplayGenerator
+from pcap2packetdrill.generator.sctp_generator import SCTPReplayGenerator
+from pcap2packetdrill.generator.replay_manager import ReplayManager
+
+__all__ = [
+    'TCPReplayGenerator',
+    'SCTPReplayGenerator',
+    'ReplayManager',
+]
