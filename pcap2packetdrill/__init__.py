b04d69c62656af33c8470a8d47dbdbf32d64f64e
Martin Becke <Martin.Becke@HAW-Hamburg.de>
2025-04-11 10:50:41 +0200

    Update __init__.py to focus on TCP only

--- pcap2packetdrill/__init__.py
+++ pcap2packetdrill/__init__.py
@@ -2,7 +2,7 @@
 PCAP2Packetdrill: Convert PCAP files to Packetdrill test scripts.
 
 This tool helps network engineers and developers convert packet captures into 
-reproducible Packetdrill test scripts for UDP, TCP, and SCTP protocols.
+reproducible Packetdrill test scripts for TCP protocol.
 """
 
 from pcap2packetdrill.protocols import SUPPORTED_PROTOCOLS
